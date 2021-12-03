e461840174f8848e8097d68d9bc0eff882681f31
Luke de Munk <luke.de.munk@hva.nl>
2021-12-03 02:24:18 +0100

    added commands
    now can login as a superuser to have more permission.

--- hackableEspDevice/SerialCommandExecuter.cpp
+++ hackableEspDevice/SerialCommandExecuter.cpp
@@ -16,7 +16,23 @@
 */
 /**************************************************************************/
 SerialCommandExecuter::SerialCommandExecuter() {
+  _isLoggedIn = false;
+  Serial.println("Serial commands available. Typ 'help' for help.");
+}
 
+/**************************************************************************/
+/*!
+  @brief    brief
+  @param    var           desc
+  @return   var           desc
+*/
+/**************************************************************************/
+void SerialCommandExecuter::setUsers(String* users, uint8_t numUsers) {
+  /* Copy users */
+    for (uint8_t i = 0; i < MAX_NUMBER_USERS*USER_INFO_LENGTH; i++) {
+      _users[i] = users[i];
+    }
+    _numberUsers = numUsers;
 }
 
 /**************************************************************************/
@@ -29,7 +45,15 @@ SerialCommandExecuter::SerialCommandExecuter() {
 void SerialCommandExecuter::executeCommand() {
     String command = Serial.readString();
     if (command != "") {
-        _parseCommand(command);
+        if (_isLoggedIn) {
+          Serial.print("~# ");
+        } else {
+          Serial.print("~$ ");
+        }
+        Serial.print(command);//echo command
+        if (!_parseCommand(command)) {
+            //debugln("Parse error");
+        }
     }
 }
 
@@ -61,14 +85,23 @@ bool SerialCommandExecuter::_parseCommand(String commandString) {
     if (command == COMMAND_1) {
       _printHelp();
     } else if (command == COMMAND_2) {
-        if (numParams < 1) {
-            Serial.println(ERROR_3_TEXT);
+        if (!_checkParams(numParams, 1) || !_enableDebug(params[0])) {
             return false;
-        } else if (numParams > 1) {
-          Serial.println(ERROR_1_TEXT);
+        }
+    } else if (command == COMMAND_3) {
+        if (!_checkParams(numParams, 1) || !_superUserLogin(params[0])) {
+            return false;
+        }
+    } else if (command == COMMAND_4) {
+        if (!_viewKey()) {
           return false;
         }
-        _enableDebug(params[0]);
+    } else if (command == COMMAND_5) {
+        _restart();
+    } else if (command == COMMAND_6) {
+        if (!_viewUsers()) {
+          return false;
+        }
     } else {
         Serial.println(ERROR_2_TEXT);
         return false;
@@ -104,7 +137,7 @@ String* SerialCommandExecuter::_trimCommand(String commandString) {
     }
     
     if (numParams-1 > MAX_NUMBER_PARAMS) {
-      Serial.println(ERROR_1_TEXT);
+      //Serial.println(ERROR_1_TEXT);
       numParams == MAX_NUMBER_PARAMS;
     }
 
@@ -118,7 +151,7 @@ String* SerialCommandExecuter::_trimCommand(String commandString) {
             }
         }
         
-        while (commandString[c] != ' ' && commandString[c] != '-' && commandString[c] != '\n' && c < commandString.length()) {
+        while (commandString[c] != ' ' && commandString[c] != '-' && commandString[c] != '\n'/* && c < commandString.length()*/) {
           item += commandString[c];
           c++;
         }
@@ -130,16 +163,15 @@ String* SerialCommandExecuter::_trimCommand(String commandString) {
     return commandItems;
 }
 
-/**************************************************************************/
-/*!
-  @brief    brief Example command 'enableDebug -1'
-  @param    var           desc
-*/
-/**************************************************************************/
-void SerialCommandExecuter::_printHelp() {
-  Serial.println("-----------------------HELP-----------------------");
-  Serial.println("A command consists of the command and sometimes arguments.");
-  Serial.println("Arguments can be given with a '-' prefix, like 'enableDebug -x'.");
+bool SerialCommandExecuter::_checkParams(uint8_t numParams, uint8_t checkValue) {
+  if (numParams < checkValue) {
+      Serial.println(ERROR_4_TEXT);
+      return false;
+  } else if (numParams > checkValue) {
+    Serial.println(ERROR_1_TEXT);
+    return false;
+  }
+  return true;
 }
 
 /**************************************************************************/
@@ -148,7 +180,26 @@ void SerialCommandExecuter::_printHelp() {
   @param    var           desc
 */
 /**************************************************************************/
-void SerialCommandExecuter::_enableDebug(String enable) {
+void SerialCommandExecuter::_printHelp() {
+  Serial.println("|---------------------------HELP---------------------------|");
+  Serial.println("A command consists of the command and sometimes arguments.");
+  Serial.println("Arguments can be given with a '-' prefix, like");
+  Serial.println("'enableDebug -x'.");
+  Serial.println("");
+  Serial.println("Available commands:");
+  Serial.println("help");
+  Serial.println("enableDebug -x -> x = 0 = off, x = 1 = on");
+  Serial.println("su -password");
+  Serial.println("viewKey");
+}
+
+/**************************************************************************/
+/*!
+  @brief    brief Example command 'enableDebug -1'
+  @param    var           desc
+*/
+/**************************************************************************/
+bool SerialCommandExecuter::_enableDebug(String enable) {
   if (enable == "1") {
     //debug = true;
     debugln("debug = true");
@@ -157,5 +208,85 @@ void SerialCommandExecuter::_enableDebug(String enable) {
     debugln("debug = false");
   } else {
     Serial.println(ERROR_3_TEXT);
+    return false;
   }
+  return true;
+}
+
+/**************************************************************************/
+/*!
+  @brief    brief Example command 'enableDebug -1'
+  @param    var           desc
+*/
+/**************************************************************************/
+bool SerialCommandExecuter::_superUserLogin(String password) {
+  if (password == "pw") {
+    _isLoggedIn = true;
+    debugln("You are now super user.");
+  } else {
+    Serial.println(ERROR_5_TEXT);
+    return false;
+  }
+  return true;
+}
+
+/**************************************************************************/
+/*!
+  @brief    brief Example command 'enableDebug -1'
+  @param    var           desc
+*/
+/**************************************************************************/
+bool SerialCommandExecuter::_viewKey() {
+  if (!_isLoggedIn) {
+      Serial.println(ERROR_6_TEXT);
+      return false;
+  }
+  Serial.println("Encryption key for config.conf");
+  return true;
+}
+
+/**************************************************************************/
+/*!
+  @brief    brief Example command 'enableDebug -1'
+  @param    var           desc
+*/
+/**************************************************************************/
+void SerialCommandExecuter::_restart() {
+  Serial.print("Restarting in ");
+  for (uint8_t s = 3; s > 0; s--) {
+    Serial.print(s);
+    Serial.print(" ");
+    delay(1000);
+  }
+  ESP.restart();
+}
+
+/**************************************************************************/
+/*!
+  @brief    brief Example command 'enableDebug -1'
+  @param    var           desc
+*/
+/**************************************************************************/
+bool SerialCommandExecuter::_viewUsers() {
+  String userPrints[USER_INFO_LENGTH] = "";
+    
+  if (!_isLoggedIn) {
+      Serial.println(ERROR_6_TEXT);
+      return false;
+  }
+  
+  Serial.println("|-USERNAME------|-PASSWORD------|-ROLE--|");
+
+  for (uint8_t i = 0; i < _numberUsers; i += 3) {
+      userPrints[0] = _users[i].c_str();                      //username
+      if(atoi(_users[i+2].c_str()) == PERMISSION_LVL_USER) {
+          userPrints[1] = _users[i+1].c_str();                //password
+          userPrints[2] = "User";
+      } else if (atoi(_users[i+2].c_str()) == PERMISSION_LVL_ADMIN) {
+          userPrints[1] = "******";
+          userPrints[2] = "Admin";
+      }
+      Serial.printf("| %s\t| %s\t| %s\t|\n", userPrints[0].c_str(), userPrints[1].c_str(), userPrints[2].c_str());
+  }
+  return true;
 }
