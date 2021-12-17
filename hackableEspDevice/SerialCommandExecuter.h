a96eed4aadbe744c210203fddd1c663e6b100563
Luke de Munk <luke.de.munk@hva.nl>
2021-12-17 23:54:26 +0100

    Added commands needed for buffer overflow

--- hackableEspDevice/SerialCommandExecuter.h
+++ hackableEspDevice/SerialCommandExecuter.h
@@ -12,23 +12,32 @@
 #include "config.h"                                                         //For the configuration.
 #include "Debugger.h"                                                       //For handling debug messages
 #include "HostnameWrite.h"
+#include "BufferOverflow.h"
 
 #define MAX_NUMBER_PARAMS  2
 
 #define COMMAND_1       "help"
 #define COMMAND_2       "enableDebug"
 #define COMMAND_3       "su"
-#define COMMAND_4       "keys"
+#define COMMAND_4       "sshkeys"
 #define COMMAND_5       "reboot"
 #define COMMAND_6       "users"
 #define COMMAND_7       "hostname"
 
+/* Used for buffer overflow */
+#define COMMAND_8       "ls"
+#define COMMAND_9       "vi"
+#define COMMAND_10      "./testprogram"
+#define COMMAND_11      "objdump"
+
 #define ERROR_1_TEXT    "Too many arguments. Typ 'help' for help."
-#define ERROR_2_TEXT    "Wrong command. Typ 'help' for help."
+#define ERROR_2_TEXT    "Bash: command not found. Typ 'help' for help."
 #define ERROR_3_TEXT    "Wrong argument(s). Add '-h' or '--help' to the command for help."
 #define ERROR_4_TEXT    "Too few arguments. Typ 'help' for help."
 #define ERROR_5_TEXT    "Wrong password."
 #define ERROR_6_TEXT    "You are no super user. Use 'su {password}' to log in."
+#define ERROR_7_TEXT    "Not able to open this file."
+#define ERROR_8_TEXT    "Too many arguments. Typ 'help' for help."
 
 class SerialCommandExecuter
 {
